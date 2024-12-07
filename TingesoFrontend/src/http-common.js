e62a40856c6e8412406ffd0ede729169a2e27dec
Sofía Gacitúa <sofia.gacitua@usach.cl>
2024-12-07 18:06:18 -0300

    Cambios en frontend para ms users (funcionando)

--- TingesoFrontend/src/http-common.js
+++ TingesoFrontend/src/http-common.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
-const BackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
+const BackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER; // localhost o ip
+const BackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT; // puerto del backend
 
 export default axios.create({
-    baseURL: `http://${BackendServer}:${BackendPort}`,
+    baseURL: `http://localhost:8088`,
     headers: {
         'Content-Type': 'application/json'
     }
