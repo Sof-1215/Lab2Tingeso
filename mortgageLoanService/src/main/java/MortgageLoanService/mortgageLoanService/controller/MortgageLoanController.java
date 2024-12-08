6dabda51443d04e5eff5d0a6154f9bf13353a510
Sofía Gacitúa <sofia.gacitua@usach.cl>
2024-12-08 00:00:13 -0300

    loanSolicitudeService funcionando localmente

--- mortgageLoanService/src/main/java/MortgageLoanService/mortgageLoanService/controller/MortgageLoanController.java
+++ mortgageLoanService/src/main/java/MortgageLoanService/mortgageLoanService/controller/MortgageLoanController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/mortgageloan")
-@CrossOrigin("*")
 public class MortgageLoanController {
     @Autowired
     MortgageLoanService mortgageLoanService;
